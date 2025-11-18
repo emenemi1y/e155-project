85fa6463cc50a98dac8ba72fa883f96c89733de7
njobanputra <njobanputra@g.hmc.edu>
2025-11-17 20:46:12 -0800

    aaa

--- mcu/STM32L432KC_FLASH.c
+++ mcu/STM32L432KC_FLASH.c
@@ -10,7 +10,7 @@
 // https://github.com/aloebs29/flash_management
 
 
-#include "C:\Users\njobanputra\Documents\GitHub\e155-project\mcu\STM32L432KC_FLASH.h"
+#include "/Users/ninajobanputra/Documents/E155Project/mcu/STM32L432KC_FLASH.h"
 
 void configureFlash() {
   FLASH->ACR |= FLASH_ACR_LATENCY_4WS;
