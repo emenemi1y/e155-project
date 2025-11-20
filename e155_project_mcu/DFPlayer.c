d60321cc8977ae0297cb33deebba48f69dd73fa3
emenemi1y <ekendrick@g.hmc.edu>
2025-11-20 14:35:37 -0800

    RFID + DFPlayer working

--- e155_project_mcu/DFPlayer.c
+++ e155_project_mcu/DFPlayer.c
@@ -19,6 +19,15 @@
 
 void initializeDF3 (USART_TypeDef * USART){
      // Initialization:
+    sendChar(USART, 0x7E);
+    sendChar(USART, 0xFF); // Version
+    sendChar(USART, 0x06); // Length
+    sendChar(USART, 0x09); // Command: Select device
+    sendChar(USART, 0x01); // Feedback
+    sendChar(USART, 0x00); // Parameter: TF card
+    sendChar(USART, 0x02); // Checksum high byte
+    sendChar(USART, 0xEF); // End byte
+    delay_units(TIM15, 50000);
       
 }
 
@@ -29,9 +38,9 @@ void playSong (USART_TypeDef * USART, uint8_t song){
       sendChar(USART, 0x12); // Command: Select device
       sendChar(USART, 0x00); // Feedback
       sendChar(USART, 0x00); // Parameter: TF card
-      sendChar(USART, 0x01); // Checksum high byte
+      sendChar(USART, song); // Checksum high byte
       sendChar(USART, 0xEF); // End byte
-      delay_units(TIM15, 2000);
+      delay_units(TIM15, 50000);
 }
 
 void pauseSong(USART_TypeDef * USART) {
@@ -43,7 +52,19 @@ void pauseSong(USART_TypeDef * USART) {
       sendChar(USART, 0x00); // Parameter: TF card
       sendChar(USART, 0x00); // Checksum high byte
       sendChar(USART, 0xEF); // End byte
-      delay_units(TIM15, 2000);
+      delay_units(TIM15, 50000);
+}
 
+void setVolume(USART_TypeDef * USART, uint8_t volume) {
+
+      sendChar(USART, 0x7E);
+      sendChar(USART, 0xFF); // Version
+      sendChar(USART, 0x06); // Length
+      sendChar(USART, 0x06); // Command: Select device
+      sendChar(USART, 0x00); // Feedback
+      sendChar(USART, 0x00); // Parameter: TF card
+      sendChar(USART, volume); // Checksum high byte
+      sendChar(USART, 0xEF); // End byte
+      delay_units(TIM15, 50000);
 
 }
--- e155_project_mcu/main.c
+++ e155_project_mcu/main.c
@@ -19,10 +19,11 @@ int main(void) {
 
   // Enable TIM15
   RCC->APB2ENR |= RCC_APB2ENR_TIM15EN;
-  initTIM(TIM15, 1e3);
+  initTIM(TIM15, 1e6);
+  initTIM(TIM16, 1e3);
 
-  //initSPI(SPI1, 3, CPOL, CPHA);
-  //PCD_Init();
+  initSPI(SPI1, 3, CPOL, CPHA);
+  PCD_Init();
   delay_units(TIM15, 40);
 
 
@@ -30,32 +31,11 @@ int main(void) {
   USART_TypeDef * USART = initUSART(USART_ID, 9600);
 
 
-  //initializeDF3(USART);
-  //while(1){
+  initializeDF3(USART);
+  //setVolume(USART, 30);
+  pauseSong(USART);
   
-  sendChar(USART, 0x7E);
-  sendChar(USART, 0xFF); // Version
-  sendChar(USART, 0x06); // Length
-  sendChar(USART, 0x09); // Command: Select device
-  sendChar(USART, 0x00); // Feedback
-  sendChar(USART, 0x00); // Parameter: TF card
-  sendChar(USART, 0x02); // Checksum high byte
-  sendChar(USART, 0xEF); // End byte
-  delay_units(TIM15, 2000);
- // }
-  while(1) {
-  sendChar(USART, 0x7E);
-  sendChar(USART, 0xFF); // Version
-  sendChar(USART, 0x06); // Length
-  sendChar(USART, 0x12); // Command: Select device
-  sendChar(USART, 0x00); // Feedback
-  sendChar(USART, 0x00); // Parameter: TF card
-  sendChar(USART, 0x01); // Checksum high byte
-  sendChar(USART, 0xEF); // End byte
-  delay_units(TIM15, 2000);
-  }
   
-  /*
   uint64_t card_id, prev_id;
   
   PCD_DumpVersionToSerial();
@@ -64,16 +44,14 @@ int main(void) {
   while(1){
     if (PICC_IsNewCardPresent()){
       if(PICC_ReadCardSerial()){
+        prev_id = card_id;
         card_id = PICC_DumpToSerial(&(uid));
       }   
-      //pauseSong(USART);
-      //if (card_id == 0x7945dc11) playSong(USART, 0x01);
-      //else if (card_id == 0x1c2ae62e) playSong(USART, 0x02);
+      pauseSong(USART);
+      if (card_id == 0x7945dc11) playSong(USART, 0x01);
+      else if (card_id == 0x1c2ae62e) playSong(USART, 0x02);
     }
-    playSong(USART, 0x01);
-    //printf("\ncurrent ID: %x", (int) card_id);
-    
   }
-  */
+  
 
 }
